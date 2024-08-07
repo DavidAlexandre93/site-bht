62071c23e955ac72e32585b5fe8ee9cefdb6faf9
Vitor Batista <vitiilimaa@gmail.com>
2024-08-07 12:55:06 -0300

    update: adição de analytics

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import AppContainer from "./components/AppContainer";
 import { LoadingContextProvider } from "./contexts/LoadingContext";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,5 +15,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <RouterProvider router={router} />
       </AppContainer>
     </LoadingContextProvider>
+    <Analytics />
   </React.StrictMode>
 );
